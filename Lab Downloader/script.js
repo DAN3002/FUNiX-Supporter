d6fede99dc20a12dd6fc9ebeda86b4895ba84341
DAN3002 <dinhanh300229@gmail.com>
2019-02-21 12:11:06 +0700

    Update Unicode in Lab Dowloader

--- Lab Downloader/script.js
+++ Lab Downloader/script.js
@@ -16,10 +16,16 @@ function download(data, filename, type) {
     }
 }
 
+$('.CodeMirror-foldgutter-open').each(function(index, el) {
+   $(el).trigger('click');
+});
+
 var text = "";
 $(".CodeMirror-code>div>.CodeMirror-line").each(function(index, el) {
     text = text + $(el).text() + "\r\n";
 });
 var title = $(".hr_tour-challenge-name").text();
 title = title.replace(/\s/g,"");
+text = text.replace(/[\u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g,'');
+text = text.replace(/[^\x00-\x7F]/g, "");
 download(text, title + ".java", "text");
